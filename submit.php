4f0ede4b304f4298c2829a17e0fa6b136687086e
pedroperalta <42123738+pedroperalta@users.noreply.github.com>
2023-03-26 19:56:13 -0300

    Update submit.php
    Nota: En este ejemplo, las respuestas correctas están codificadas directamente en el archivo PHP. En una aplicación real, las respuestas correctas se almacenarían en una base de datos o archivo separado y se recuperarían dinámicamente en lugar de ser codificadas directamente en el archivo PHP.

--- submit.php
+++ submit.php
@@ -1,16 +1,27 @@
-<?php
-// Conexión a la base de datos
-$db_host = "localhost";
-$db_user = "usuario";
-$db_password = "contraseña";
-$db_name = "nombre_base_de_datos";
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
-
-// Verificar si se envió un formulario y procesar la respuesta
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $question_id = $_POST["question_id"];
-  $answer = $_POST["answer"];
-  $sql = "INSERT INTO respuestas (question_id, answer) VALUES ('$question_id', '$answer')";
-  $mysqli->query($sql);
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Retroalimentación de respuesta</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Retroalimentación de respuesta</h1>
+    <?php
+      $answers = array(
+        1 => 'b',
+        2 => 'c'
+      );
+      $question_id = $_POST['question_id'];
+      $selected_answer = $_POST['answer'];
+      if ($selected_answer == $answers[$question_id]) {
+        echo '<p class="correct">¡Correcto!</p>';
+      } else {
+        echo '<p class="incorrect">Respuesta incorrecta. La respuesta correcta es ' . strtoupper($answers[$question_id]) . '.</p>';
+      }
+    ?>
+    <p><a href="index.html">Volver a las preguntas</a></p>
+  </div>
+</body>
+</html>
