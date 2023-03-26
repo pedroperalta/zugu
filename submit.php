0034b8baabd571d1604585c6eb2889b7620cc1aa
pedroperalta <42123738+pedroperalta@users.noreply.github.com>
2023-03-26 20:07:07 -0300

    Update submit.php
    Para implementar el proceso de mostrar las preguntas una por una, podemos usar un archivo de control PHP que mantenga el registro de qué pregunta se está mostrando actualmente. Por ejemplo, podemos crear un archivo quiz.php que incluya el siguiente código:
    
    Este código define una lista de preguntas y mantiene un índice que indica cuál es la pregunta actual. Si se recibe una respuesta, se valida y se avanza a la siguiente pregunta si es correcta, o se muestra la misma pregunta de nuevo si es incorrecta. Si se llega al final de las preguntas, se muestra un mensaje de finalización. Al final de cada pregunta, se guarda el índice actual en la sesión para que se mantenga en la siguiente página

--- submit.php
+++ submit.php
@@ -1,27 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Retroalimentación de respuesta</title>
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-  <div class="container">
-    <h1>Retroalimentación de respuesta</h1>
-    <?php
-      $answers = array(
-        1 => 'b',
-        2 => 'c'
-      );
-      $question_id = $_POST['question_id'];
-      $selected_answer = $_POST['answer'];
-      if ($selected_answer == $answers[$question_id]) {
-        echo '<p class="correct">¡Correcto!</p>';
-      } else {
-        echo '<p class="incorrect">Respuesta incorrecta. La respuesta correcta es ' . strtoupper($answers[$question_id]) . '.</p>';
-      }
-    ?>
-    <p><a href="index.html">Volver a las preguntas</a></p>
-  </div>
-</body>
-</html>
+<?php
+session_start();
+
+// Definir la lista de preguntas
+$questions = array(1, 2, 3, 4, 5);
+
+// Obtener el índice de la pregunta actual
+$question_index = isset($_SESSION['question_index']) ? $_SESSION['question_index'] : 0;
+
+// Si se recibió una respuesta, validarla y avanzar a la siguiente pregunta
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $question_id = $_POST['question_id'];
+  $answer = $_POST['answer'];
+
+  // Validar la respuesta
+  if (validate_answer($question_id, $answer)) {
+    // Respuesta correcta, avanzar a la siguiente pregunta
+    $question_index++;
+  }
+  else {
+    // Respuesta incorrecta, mostrar la misma pregunta de nuevo
+    $question_index = $question_index;
+  }
+}
+
+// Si se llegó al final de las preguntas, mostrar un mensaje de finalización
+if ($question_index >= count($questions)) {
+  echo "<h1>¡Felicidades, ha completado el cuestionario!</h1>";
+  exit();
+}
+
+// Mostrar la pregunta actual
+$question_id = $questions[$question_index];
+include "index$question_id.html";
+
+// Guardar el índice de la pregunta actual en la sesión
+$_SESSION['question_index'] = $question_index;
+
+// Función para validar la respuesta
+function validate_answer($question_id, $answer) {
+  // Definir las respuestas correctas
+  $answers = array(
+    1 => 'b',
+    2 => 'c',
+    3 => 'd',
+    4 => 'b',
+    5 => 'a'
+  );
+
+  // Validar la respuesta
+  return isset($answers[$question_id]) && $answers[$question_id] === $answer;
+}
